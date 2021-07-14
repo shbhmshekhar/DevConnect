a55e1df48d0755846567e8bf013a896bc6702f3b
Shubham Shekhar <shbhmshekhar@outlook.com>
2021-07-14 17:48:08 +0530

    Added Existing User Check

---
+++ routes/api/users.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/Users');
+const gravatar = require('gravatar');
+
+const bcrypt = require('bcryptjs');
+
+
+// @route  POST api/user
+// @desc    Register User
+// @access  Public
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+
+    try {
+        //Check User exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                errors: [{ msg: 'User already exists' }]
+            });
+        }
+
+        //Get user Gravitar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+        //Encrypt USer password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        //return jsonWebToken
+        res.send('User Registered');
+    } catch (error) {
+        console.log(error.message);
+        res.send(500).send('Server Error');
+    }
+
+
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const connectDB = require('./Config/db');
 
 const app = express();
+
+//Connect DB
+
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('API Running');
 })
+
+//Define Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

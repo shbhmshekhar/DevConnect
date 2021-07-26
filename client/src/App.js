59f025b0dd0ca57fdc0db787cc531718cbb32048
Shubham Shekhar <shbhmshekhar@outlook.com>
2021-07-26 15:03:51 +0530

    Front end in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import Navbar from './Components/Layout/Navbar';
+import Landing from './Components/Layout/Landing';
+import Login from './Components/auth/Login';
+import Register from './Components/auth/Register';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
--- server.js
+++ server.js
@@ -13,15 +13,14 @@ app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('API Running');
-})
+  res.send('API Running');
+});
 
 //Define Routes
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/post', require('./routes/api/post'));
-
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
